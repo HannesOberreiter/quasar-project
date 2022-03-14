ea924ed20de1669a02fc2012e77c2fdd06c85fb4
HannesOberreiter <hoberreiter@gmail.com>
2022-03-14 14:23:13 +0100

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    {{ test.name }}
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import test from 'assets/test.json';
+</script>
